735835934903df73fddbad7989943451fe5c0515
DepFA <35278260+dfaker@users.noreply.github.com>
2021-06-21 13:18:59 +0100

    Create discoautpplay.js

---
+++ discoautpplay.js
@@ -0,0 +1,32 @@
+setInterval(function(){ 
+	var videoList = document.getElementsByTagName('video');
+	var apcheckbox = document.getElementById('apVideocheck');
+	var aploopcheckbox = document.getElementById('apLoopcheck');
+	if(!apcheckbox){
+		var apdiv = document.createElement('div');
+		apdiv.style.zIndex = '999';
+		apdiv.style.color = 'white';
+		apdiv.style.float = 'left';
+		apdiv.style.position = 'absolute';
+		apdiv.style.opacity = '0.4';
+		apdiv.style.left = '308px';
+		apdiv.style.top = '19px';
+		apdiv.style.fontSize = '13px';
+		apdiv.innerHTML = '<input id="apVideocheck" name="apVideocheck" type="checkbox">  <label for="apVideocheck">AutoPlay New Video</label>  <input id="apLoopcheck" ame="apLoopcheck" type="checkbox"> <label for="apLoopcheck">Loop Videos</label>  ';
+		document.body.appendChild(apdiv);
+	}
+
+	var last;
+	for (var vi = 0; vi < videoList.length; vi++) {
+		last=videoList[vi];
+		if(aploopcheckbox && aploopcheckbox.checked && !last.loop){
+			last.loop=true;
+		}else if(aploopcheckbox && !aploopcheckbox.checked && last.loop==true){
+			last.loop=false;
+		}
+	}
+	if(apcheckbox && apcheckbox.checked && last && !last.className.includes('autoPlayed') && last.parentElement && last.parentElement.childElementCount==4){
+		last.classList.add("autoPlayed");
+		last.click();
+	}
+ }, 100);
