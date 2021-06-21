f2cff4f8cc2961fd4a2d5f84360ce98182592f35
DepFA <35278260+dfaker@users.noreply.github.com>
2021-06-21 14:12:06 +0100

    Update discoautpplay.js

--- discoautpplay.js
+++ discoautpplay.js
@@ -1,8 +1,17 @@
 
 
+function isScrolledIntoView(el) {
+	var rect = el.getBoundingClientRect();
+	var elemTop = rect.top;
+	var elemBottom = rect.bottom;
+	var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+	return isVisible;
+}
+
 setInterval(function(){ 
 	var videoList = document.getElementsByTagName('video');
 	var apcheckbox = document.getElementById('apVideocheck');
+	var apVischeckbox = document.getElementById('apVisVideocheck');
 	var aploopcheckbox = document.getElementById('apLoopcheck');
 	if(!apcheckbox){
 		var apdiv = document.createElement('div');
@@ -16,6 +25,10 @@ setInterval(function(){
 		apdiv.style.fontSize = '13px';
 		apdiv.innerHTML =  '<input id="apVideocheck" name="apVideocheck" type="checkbox">';
 		apdiv.innerHTML += '<label for="apVideocheck">AutoPlay New Video</label>';
+
+		apdiv.innerHTML +=  '<input id="apVisVideocheck" name="apVisVideocheck" type="checkbox">';
+		apdiv.innerHTML += '<label for="apVisVideocheck">AutoPlay All Visible</label>';
+
 		apdiv.innerHTML += '<input id="apLoopcheck" ame="apLoopcheck" type="checkbox">'
 		apdiv.innerHTML += '<label for="apLoopcheck">Loop Videos</label>';
 		document.body.appendChild(apdiv);
@@ -29,6 +42,27 @@ setInterval(function(){
 				last.loop=true;
 			}else if(aploopcheckbox && !aploopcheckbox.checked && last.loop==true){
 				last.loop=false;
+			}
+		}
+		if(apVisVideocheck && apVisVideocheck.checked && last.parentElement && last.parentElement.childElementCount==4){
+			if(isScrolledIntoView(last)){
+				if(!last.className.includes('autoPlayed') && last.paused){
+					last.classList.add("autoPlayed");
+					last.click();
+				}else if(last.paused){
+					last.play();
+				}
+			}else if(!last.paused){
+				last.pause()
+			}
+		}
+		if(apVisVideocheck && apVisVideocheck.checked && last.parentElement && last.parentElement.childElementCount!=4){
+			if(isScrolledIntoView(last)){
+				if(last.paused){
+					last.play();
+				}
+			}else if(!last.paused){
+				last.pause()
 			}
 		}
 	}
